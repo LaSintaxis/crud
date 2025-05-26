2bb5d7b79b1e2628cd1294bf2db92abfde11e7ef
LaSintaxis <nataliamariacardenasgarcia@gmail.com>
2025-05-26 17:23:26 -0500

    se añade archivo de productos y usuario en models. se añaden rutas de productos en routes

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,25 +1,25 @@
-const product = require('../models/Product')
+const Product = require('../models/Product')
 const Category = require('../models/category')
 const subcategory = require('../models/Subcategory');
 const Subcategory = require('../models/Subcategory');
 
 exports.createProduct = async (req, res) => {
     try {
-        const {name, description, price, stock, category, subcategory} = req.body;
+        const { name, description, price, stock, category, subcategory } = req.body;
 
         //validacion de campos requeridos
         if (!name || !description || !price || !stock || !category || !subcategory) {
             return res.status(400).json({
-                success:false,
+                success: false,
                 message: 'Todos los campos son obligatorios'
             })
         }
 
         //Verificar que la categoria exista
         const categoryExists = await Category.findById(category)
-        if(!categoryExists){
+        if (!categoryExists) {
             return res.status(404).json({
-                success:false,
+                success: false,
                 message: 'la categoria especifica no existe'
             })
         }
@@ -30,7 +30,7 @@ exports.createProduct = async (req, res) => {
             category: category
         })
 
-        if(!subcategoryExists){
+        if (!subcategoryExists) {
             return res.status(400).json({
                 success: false,
                 message: 'La subcategoria no existe o no pertenece a la categoria especifica'
@@ -48,8 +48,8 @@ exports.createProduct = async (req, res) => {
             // CreatedBy se agregara despues de verificar el usuario
         })
 
-        ///Verificar si el usuario esta disponivle en el request
-        if(req.user && req.user.id){
+        ///Verificar si el usuario esta disponible en el request
+        if (req.user && req.user.id) {
             product.createdBy = req.user.id;
         }
 
@@ -58,47 +58,173 @@ exports.createProduct = async (req, res) => {
 
         //Obtener el producto con los datos poblados
         const productWithDetails = await Product.findById(savedProduct._id)
-        .populate('category', 'name')
-        .populate('subcategory', 'name' );
+            .populate('category', 'name')
+            .populate('subcategory', 'name');
 
         res.status(201).json({
             success: true,
             message: ('producto creado exitosamente'),
             data: productWithDetails
         })
-    }catch(error){
+    } catch (error) {
         console.error('error en createdProduct: ', error)
 
         //manejo de errores en mongodb
-        if(error.code === 11000) {
+        if (error.code === 11000) {
             return res.status(400).json({
-                success:false,
+                success: false,
                 message: 'Ya existe un producto con ese nombre'
             })
         }
         res.status(500).json({
-            success:false,
-            message: 'Error al crear el producto', 
+            success: false,
+            message: 'Error al crear el producto',
             error: error.message
         })
     }
-    
+
 }
 
 //Consulta de productos GET api/products
 exports.getProducts = async (req, res) => {
     try {
         const products = await Product.find()
-        .populate('category', 'name')
-        .populate('subcategory', 'name')
-        .sort({createAt: -1});
+            .populate('category', 'name')
+            .populate('subcategory', 'name')
+            .sort({ createAt: -1 });
 
         res.status(200).json({
             success: true,
             count: products.length,
             data: products
         })
-    } catch(error){
-        
+    } catch (error) {
+        console.error('Error de getProducts', error);
+        res.status(500).json({
+            success: false,
+            message: 'Error al obtener los productos'
+        })
+    }
+}
+
+
+exports.getProductsById = async (res, req) => {
+    try {
+        const product = await Product.findById(req.params.id)
+            .populate('category', 'name description')
+            .populate('subcategory', 'name description')
+
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: 'producto no encontrado'
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: product
+        })
+    } catch (error) {
+        console.error('error en getProductById', error);
+        res.status(500).json({
+            success: false,
+            message: 'Errpr al obtener el producto'
+        });
+    };
+};
+
+exports.updateProduct = async (req, res) => {
+    try {
+        const { name, description, price, stock, category, subcategory } = req.body;
+        const updateData = {};
+
+        //Validar y preparar datos para act
+        if (name) updateData.name = name;
+        if (description) updateData.description = description;
+        if (price) updateData.price = price;
+        if (stock) updateData.stock = stock;
+
+        //validar relaciones
+        if (category || subcategory) {
+            if (category) {
+                const categoryExists = await Category.findById(category);
+                if (!categoryExists) {
+                    return res.status(404).json({
+                        success: false,
+                        message: 'La categoria especifica no existe'
+                    })
+                }
+
+                updateData.category = category;
+            }
+
+            if (subcategory) {
+                const subcategoryExists = await Subcategory.findOne({
+                    _id: subcategory,
+                    category: category || updateData.category
+                })
+
+                if (!subcategoryExists) {
+                    return res.status(400).json({
+                        success: false,
+                        message: 'La subcategoria no existe o no pertenece a la categoria'
+                    })
+                }
+                updateData.subcategory = subcategory
+            }
+        }
+
+        //Actualizar el producto
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            updateData,
+            {
+                new: true,
+                runValidators: true
+            }
+        )
+            .populate('category', 'name')
+            .populate('subcategory', 'name')
+
+            if (!updatedProduct) {
+                return res.status(404).json({
+                    success: false,
+                    message: 'Producto no encontrado'
+                })
+            }
+            res.status(200).json({
+                success: true,
+                message: 'Producto actualizado exitosamente',
+                data: updatedProduct
+            });
+    } catch (error) {
+        console.error('Error en updatedProduct')
+
+
+    }
+}
+
+exports.deleteProduct = async (req, res) => {
+    try{
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if(!product){
+            return res.status(404).json({
+                success:false,
+                message: 'Producto no encontrado'       
+            })
+        }
+
+        res.status(200).json({
+            success:true,
+            message: ' Producto eliminado exitosamente',
+            data: product
+        })
+    }catch(error){
+        console.error('Error en deleteProduct: ',error);
+        res.status(500).json({
+            success:false,
+            message:'Error al eliminat el producto'
+        })
     }
 }
---
+++ models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+    user: {
+        type: String,
+        required:true,
+        unique: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    password: {
+        type: String,
+        required:true,
+        minlength: 6,
+        select:false
+    },
+    role:{
+        type: String,
+        default: 'auxiliar',
+        enum: ['admin','coordinador','auxiliar']
+    }
+},{
+    timestamps: true,
+    versionKey: false
+})
+
+// hash de contraseña antes de guardar
+userSchema.pre('save', async function(next){
+    if (!this.isModified('password')) return next();
+
+    try{
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt)
+        next()
+    } catch (error) {
+        next(error);
+    }
+})
+
+module.exports = mongoose.model('user',userSchema)
